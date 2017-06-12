12545eb9ca42bc77b0dcd2ffcde6781d6d7fea45
Mike Hagerty <m.hagerty@isti.com>
2017-06-12 15:02:55 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+liblog
+
+Small wrapper to Python built-in logging to auto-initialize logger handlers and format
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='mth_utils',
+    version='0.1',
+    description='Put small utility helpers in here',
+    url='http://example.com',
+    author='MTH',
+    author_email='m.hagerty@isti.com',
+    license='GPL',
+    packages=['mth_utils'],
+    zip_safe=False,
+)
